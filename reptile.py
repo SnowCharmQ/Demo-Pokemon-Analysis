533af8b6a7a7aacfc360f5a8bd08d549f87dd82c
SnowCharm <619022098@qq.com>
2023-01-13 14:05:51 +0800

    store html in file

--- reptile.py
+++ reptile.py
@@ -1,7 +1,14 @@
+import os
 from urllib import request
 
 url = "https://pokemon.fandom.com/wiki/List_of_Pok%C3%A9mon"
 res = request.urlopen(url)
 
 html = res.read()
-print(html)
+html = html.decode()
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+with open("data/pokemon.txt", mode='w', encoding='utf-8') as f:
+    f.write(html)
+f.close()
