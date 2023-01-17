1a8cfed297a32e9d827b723b0bfebc703d882fe9
SnowCharm <619022098@qq.com>
2023-01-17 13:15:46 +0800

    show the current time in the page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import *
+
+import utils
 
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/time')
+def get_time():
+    return utils.get_time()
 
 
 if __name__ == '__main__':
