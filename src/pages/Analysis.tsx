1914ae72b66cba52b85abf0516dce12b97a112ed
SnowCharm <619022098@qq.com>
2023-08-02 21:37:07 +0800

    :sparkles: add title

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import '../styles/Analysis.css'
+import Title from './Title'
 
 export default function Analysis() {
   return (
-    <div>Analysis</div>
+    <div className='analysis'>
+      <div className='title'>
+        <Title />
+      </div>
+    </div>
   )
 }
